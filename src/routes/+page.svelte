c7e2bada60b8774cfee24e7bab2fb287b0e4e22a
Adam Rackis <arackis@spotify.com>
2026-06-20 22:01:23 -0400

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <h1>Welcome to SvelteKit 5</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation 2</p>
 
 <div class="flex flex-col gap-2">
   {#each books as book}
